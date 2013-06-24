0181d3d3579213a846d5290c5c3a53cb1b62e244
reinei <sonicfuchs@gmail.com>
2013-06-24 20:56:06 +0200

    Just toying around

--- common/reinei/CD/events/CommandEventHandler.java
+++ common/reinei/CD/events/CommandEventHandler.java
@@ -3,6 +3,7 @@ package reinei.CD.events;
 import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.EventPriority;
 import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.ServerChatEvent;
 
 public class CommandEventHandler
 {
@@ -10,5 +11,14 @@ public class CommandEventHandler
 	public void CommandIncoming(CommandEvent ev)
 	{
 		System.out.println(ev.command.getCommandName());
+	}
+	
+	@ForgeSubscribe(priority = EventPriority.HIGHEST)
+	public void CommandIncoming(ServerChatEvent ev)
+	{
+		if (ev.message.startsWith("/&"))
+		{
+			System.out.println("Received a Message with the right signature.");
+		}
 	}
 }
